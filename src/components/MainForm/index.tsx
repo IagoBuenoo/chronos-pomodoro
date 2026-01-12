f4c5b73e3652ee86227b71b5ad3afe7812502f51
Iago Bueno <iagobuenoo@outlook.com>
2026-01-12 15:51:37 -0300

    Created MainTemplate and started creating project pages

---
+++ src/components/MainForm/index.tsx
@@ -0,0 +1,34 @@
+import { PlayCircleIcon } from 'lucide-react';
+
+import { DefaultButton } from '../DefaultButton';
+import { Cycles } from '../Cycles';
+import { DefaultInput } from '../DefaultInput';
+
+import styles from './styles.module.css';
+
+export function MainForm() {
+  return (
+    <form className={styles.form} action=''>
+      <div className={styles.formRow}>
+        <DefaultInput
+          id='input'
+          labelText='Task'
+          type='text'
+          placeholder='Digite algo'
+        />
+      </div>
+
+      <div className={styles.formRow}>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+
+      <div className={styles.formRow}>
+        <Cycles />
+      </div>
+
+      <div className={styles.formRow}>
+        <DefaultButton icon={<PlayCircleIcon />} />
+      </div>
+    </form>
+  );
+}
