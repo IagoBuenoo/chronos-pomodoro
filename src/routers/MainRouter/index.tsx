0392970e99a9fc055b94c6d93fa322a71b730a45
Iago Bueno <iagobuenoo@outlook.com>
2026-01-26 15:14:15 -0300

    Created History page with formatted date and sort task options

--- src/routers/MainRouter/index.tsx
+++ src/routers/MainRouter/index.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes, useLocation } from 'react-router';
 import { NotFound } from '../../pages/NotFound';
 import { AboutPomodoro } from '../../pages/AboutPomodoro';
 import { Home } from '../../pages/Home';
+import { History } from '../../pages/History';
 import { useEffect } from 'react';
 
 function ScrollToTop() {
@@ -21,6 +22,7 @@ export function MainRouter() {
         <Route path='/' element={<Home />} />
         <Route path='/about-pomodoro/' element={<AboutPomodoro />} />
         <Route path='*' element={<NotFound />} />
+        <Route path='/history/' element={<History />} />
       </Routes>
       <ScrollToTop />
     </BrowserRouter>
